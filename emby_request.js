49297a38d480b5df37cc1e72ea9a984fd05ea5cf
Gluvyi <60965636+Gluvyi@users.noreply.github.com>
2023-07-31 15:48:05 +0800

    更新 emby_request.js

--- emby_request.js
+++ emby_request.js
@@ -1,6 +1,5 @@
 const headers = $request.headers;
 delete headers["user-agent"];
-// delete headers["X-Emby-Authorization"];
 
 headers['User-Agent'] = `Fileball/200 CFNetwork/1327.0.4 Darwin/21.2.0`;
 
@@ -8,6 +7,5 @@ if ('X-Emby-Authorization' in headers){
     delete headers["X-Emby-Authorization"];
     headers['X-Emby-Authorization'] = `Emby UserId=71B51549-82C2-4783-AF1E-DB19302A8B07,Client=Filebox,Device=iPhone,DeviceId=F499F2DA-A0A0-4C07-B423-996CDEA6EC22,Version=1.2.27`;
 }
-// headers['X-Emby-Authorization'] =  `Emby UserId=71B51549-82C2-4783-AF1E-DB19302A8B07,Client=Filebox,Device=iPhone,DeviceId=F499F2DA-A0A0-4C07-B423-996CDEA6EC22,Version=1.2.27`;
 
 $done({ headers });

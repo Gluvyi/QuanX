219e28a58b04d99a0c50720a53fc3c9708d64b9c
Gluvyi <60965636+Gluvyi@users.noreply.github.com>
2023-10-25 22:11:51 +0800

    更新 jd_cookie.js

--- jd_cookie.js
+++ jd_cookie.js
@@ -58,7 +58,7 @@ function getUsername(ck) {
 
 async function getScriptUrl() {
   const response = await $.http.get({
-    url: "https://raw.githubusercontent.com/dompling/Script/master/jd/ql_api.js",
+    url: "https://raw.githubusercontent.com/Gluvyi/QuanX/main/ql_api.js",
   });
   return response.body;
 }
@@ -179,19 +179,65 @@ function updateJDHelp(username) {
 
 async function GetCookie() {
   const CV = `${$request.headers["Cookie"] || $request.headers["cookie"]};`;
-
-    
+  
   if ($request.headers && $request.url.indexOf("https://lbsgw.m.jd.com/m2") > -1) {
-    if (CV.match(/pt_key=([^=;]+?);/)[1]) {
-      const wskey = CV.match(/pt_key=([^=;]+?);/)[1];
-      const pt_pin = CV.match(/pt_pin=([^=;]+?);/)[1];
-      const code = `pt_key=${wskey};pt_pin=${pt_pin};`;
-      console.log("用户cookie为：\n" + code);
-      return $.notify(pt_pin + "获取cookie成功 🎉")
+    if (CV.match(/(pt_key=.+?pt_pin=|pt_pin=.+?pt_key=)/)) {
+      const CookieValue = CV.match(/pt_key=.+?;/) + CV.match(/pt_pin=.+?;/);
+      if (CookieValue.indexOf("fake_") > -1) return console.log("异常账号");
+      const DecodeName = getUsername(CookieValue);
+      let updateIndex = null,
+        CookieName,
+        tipPrefix;
+
+      const CookiesData = getCache();
+      const updateCookiesData = [...CookiesData];
+
+      CookiesData.forEach((item, index) => {
+        if (getUsername(item.cookie) === DecodeName) updateIndex = index;
+      });
+
+      if ($.ql) {
+        for (const item of allConfig) {
+          $.ql_config = item;
+          $.ql.initial();
+          await $.ql.asyncCookie(CookieValue, "JD_COOKIE");
+        }
       }
-  } else {
-    console.log("未匹配到相关信息，退出抓包");
-  }
+
+      if (updateIndex !== null) {
+        // const response = await TotalBean(updateCookiesData[updateIndex].cookie)
+        // if (response && response.retcode === '0')
+        //   return console.log('cookie 未过期，无需更新')
+        updateCookiesData[updateIndex].cookie = CookieValue;
+        CookieName = "【账号" + (updateIndex + 1) + "】";
+        tipPrefix = "更新京东";
+      } else {
+        updateCookiesData.push({
+          userName: DecodeName,
+          cookie: CookieValue,
+        });
+        CookieName = "【账号" + updateCookiesData.length + "】";
+        tipPrefix = "首次写入京东";
+      }
+      const cacheValue = JSON.stringify(updateCookiesData, null, `\t`);
+      $.write(cacheValue, CacheKey);
+      updateJDHelp(DecodeName);
+
+      if ($.mute === "true") {
+        return console.log(
+          "用户名: " + DecodeName + tipPrefix + CookieName + "Cookie成功 🎉"
+        );
+      }
+      $.notify(
+        "用户名: " + DecodeName,
+        "",
+        tipPrefix + CookieName + "Cookie成功 🎉",
+        { "update-pasteboard": CookieValue }
+      );
+    } else {
+      console.log("ck 写入失败，未找到相关 ck");
+    }
+  } else if ($request.headers && $request.url.indexOf("newUserInfo") > -1)
 }
 
 async function TotalBean(Cookie) {

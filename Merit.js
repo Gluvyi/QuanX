7331cee89d86cf472bcf22b5685345682fdb1f70
Gluvyi <60965636+Gluvyi@users.noreply.github.com>
2024-07-01 14:45:22 +0800

    Update Merit.js

--- Merit.js
+++ Merit.js
@@ -8,15 +8,17 @@ hostname = api.merach.com
 
 const expireData = '2026-09-30';
 
-// 当前日期
-var currentData = new Date();
+
+
 
 // 和指定日期相差天数
 function dateDiffInDays() {
-    var _MS_PER_DAY = 1000 * 60 * 60 * 24;
-    var utc1 = Date.UTC(expireData.getFullYear(), expireData.getMonth(), expireData.getDate());
-    var utc2 = Date.UTC(currentData.getFullYear(), currentData.getMonth(), currentData.getDate());
-    return Math.floor((utc2 - utc1) / _MS_PER_DAY);
+    // 当前日期
+    var currentData = new Date();
+    // 设置目标日期为2026-09-30
+    const targetDate = new Date(expireData);
+    const timeDiff = targetDate.getTime() - currentDate.getTime();
+    return Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
 }
 
 var replaceData = {

98dc1c1403536321cbf762f1132dd6f73d34560a
Gluvyi <60965636+Gluvyi@users.noreply.github.com>
2025-03-14 23:48:46 +0800

    Update LongForHomeAd.js

--- LongForHomeAd.js
+++ LongForHomeAd.js
@@ -4,7 +4,7 @@ if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
 // 去除app初始化首页中商城按钮
-if (url.includes("/supera/C4/v1_11_0/publicApi/nav/query/app")) {
+if (url.includes("/app")) {
     if (obj?.data?.data?.length > 0) {
         if (Array.isArray(obj.data.data)){
             obj.data.data = obj.data.data.filter(item => item?.frameName !== "商城")
@@ -14,7 +14,7 @@ if (url.includes("/supera/C4/v1_11_0/publicApi/nav/query/app")) {
 } 
 
 // app中只保留在社区Tab页
-if (url.includes("/supera/C4/v1_11_0/publicApi/nav/query/navigation")) {
+if (url.includes("/navigation")) {
     if (obj?.data?.data?.length > 0) {
         if (Array.isArray(obj.data.data)){
             obj.data.data = obj.data.data.filter(item => item?.frameName === "在社区")

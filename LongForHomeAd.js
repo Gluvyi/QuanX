15a249fcf52e0d812d9e316b05a0d87de4a578be
Gluvyi <60965636+Gluvyi@users.noreply.github.com>
2025-03-15 00:48:52 +0800

    Update LongForHomeAd.js

--- LongForHomeAd.js
+++ LongForHomeAd.js
@@ -7,13 +7,23 @@ let obj = JSON.parse($response.body);
 if (url.includes("/app")) {
     if (obj?.data?.data?.length > 0) {
         if (Array.isArray(obj.data.data)){
-            obj.data.data = obj.data.data.filter(item => item?.frameName !== "商城")
+            obj.data.data = obj.data.data.filter(item => item?.frameName !== "商城" && item.frameName !== "首页");
         }
+        
+        obj.data.data = obj.data.data.map(item => {
+                if (item.frameName === "服务") {
+                    return{
+                        ...item,
+                        frameCode: "home"
+                    };
+                }
+                return item;
+            });
         console.log(obj.data.data)
     }
 } 
 
-// app中只保留在社区Tab页
+// app中只保留"在社区"Tab页
 if (url.includes("/navigation")) {
     if (obj?.data?.data?.length > 0) {
         if (Array.isArray(obj.data.data)){

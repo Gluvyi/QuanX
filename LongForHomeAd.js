b995cfcc70c72558c568eefd46edc82d0a1cb953
Gluvyi <60965636+Gluvyi@users.noreply.github.com>
2025-03-15 00:53:16 +0800

    Update LongForHomeAd.js

--- LongForHomeAd.js
+++ LongForHomeAd.js
@@ -3,8 +3,10 @@ const url = $request.url;
 if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
+const urlSuffix = "/nav/query
+
 // 去除app初始化首页中商城按钮
-if (url.includes("/app")) {
+if (url.includes(urlSuffix + "/app")) {
     if (obj?.data?.data?.length > 0) {
         if (Array.isArray(obj.data.data)){
             obj.data.data = obj.data.data.filter(item => item?.frameName !== "商城" && item.frameName !== "首页");
@@ -24,13 +26,19 @@ if (url.includes("/app")) {
 } 
 
 // app中只保留"在社区"Tab页
-if (url.includes("/navigation")) {
+if (url.includes(urlSuffix + "/navigation")) {
     if (obj?.data?.data?.length > 0) {
         if (Array.isArray(obj.data.data)){
             obj.data.data = obj.data.data.filter(item => item?.frameName === "在社区")
         }
         console.log(obj.data.data)
     }
+}
+
+// 去除首页中购物推广
+if (url.includes("/getFeedData/TJ")) {
+    obj.data.feedData = [];
+    console.log(obj.data.feedData)
 }
 
 $done({ body: JSON.stringify(obj) });

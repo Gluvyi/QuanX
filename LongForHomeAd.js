62e5d405408ef67c6ec10b28c3beb05544b6b0e5
Gluvyi <60965636+Gluvyi@users.noreply.github.com>
2025-03-14 23:28:07 +0800

    Update LongForHomeAd.js

--- LongForHomeAd.js
+++ LongForHomeAd.js
@@ -7,7 +7,7 @@ if (url.includes("/supera/C4/v1_11_0/publicApi/nav/query/app")) {
     if (obj?.data?.data?.length > 0) {
         let jsonData = obj.data.data;
         if (Array.isArray(obj.data.data)){
-            obj.data.data = obj.data.data.filter(item => item?.frameName?.equals("商城"))
+            obj.data.data = obj.data.data.filter(item => item?.frameName !== equals("商城"))
         }
         console.log(obj.data.data)
     }
